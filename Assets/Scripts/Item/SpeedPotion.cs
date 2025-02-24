f7a250ad42a96a8e8ea55eab354ab93373f88a54
DevAkasha <hmy9025@gmail.com>
2025-02-24 20:07:08 +0900

    [feat] PlayerAttack 추가중
    [feat] PlayerAttack 추가중
    초록이 파랑이 완료 노랑이 해야함

--- Assets/Scripts/Item/SpeedPotion.cs
+++ Assets/Scripts/Item/SpeedPotion.cs
@@ -7,7 +7,7 @@ public class SpeedPotion : Item
     public float addSpeed = 3f;
     public float duration = 3f;
 
-    protected override void ApplyEffect(Player player)
+    protected override void ApplyEffect(PlayerAction player)
     {
         player.StartCoroutine(player.IncreaseSpeed(addSpeed, duration));
     }
