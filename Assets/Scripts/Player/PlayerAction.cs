990cf57684679ffe3a781ad581b81412731ca816
park8306 <kosaf0804@gmail.com>
2025-02-24 10:53:37 +0900

    [ feat ] 플레이어 이동 및 점프 구현

---
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerAction : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+
+    [SerializeField] private float speed;         // 플레이어 스피드
+    [SerializeField] private float jumpHeight;     // 점프 높이
+
+    [SerializeField] private float rayLength;     // Ray 길이
+
+
+    [SerializeField] private bool isGround;       // 땅에 닿아있는지 확인 
+    [SerializeField] bool checkDoubleJump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        CheckGround();
+
+        Jump();
+    }
+    
+    private void Move()
+    {
+        rigid.velocity = new Vector2(speed, rigid.velocity.y);  // 플레이어 이동
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit2D hitData;
+        hitData = Physics2D.Raycast(transform.position, Vector3.down, rayLength, LayerMask.GetMask("Ground"));
+
+        isGround = hitData.collider != null;
+
+        if (isGround == true) checkDoubleJump = true;
+    }
+
+    private void Jump()
+    {
+        // 바닥에 닿아있고 스페이스키를 눌렀을 때 점프
+        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
+            rigid.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+        // 바닥에 닿아있지 않다면 더블 점프 체크 변수를 확인하고 점프
+        else if(!isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!checkDoubleJump) return;
+
+            checkDoubleJump = false;
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
+            rigid.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.down * rayLength);
+    }
+}
