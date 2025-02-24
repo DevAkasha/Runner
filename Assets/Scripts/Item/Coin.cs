6c6fc2d70e4540936a6a3384c852af26eee8c141
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-02-24 11:58:52 +0900

    Revert "Merge branch 'dev_Yuji' into dev"
    This reverts commit b96dd48d74bf3dfd57183af407cd64b820876599, reversing
    changes made to 5ca963b76ad6d42d523d143dbd603ab9f0bdc8c2.

--- Assets/Scripts/Item/Coin.cs
+++ Assets/Scripts/Item/Coin.cs
@@ -5,21 +5,9 @@ using UnityEngine;
 public class Coin : Item
 {
     public int ScoreValue = 500;
-    public GameObject itemEffect;
 
     protected override void ApplyEffect(Player player)
     {
         GameManager.Instance.AddScore(ScoreValue);
-        PlayEffect();
-    }
-
-    public GameObject PlayEffect()
-    {
-        if(itemEffect != null)
-        {
-            return Instantiate(itemEffect, transform.position, Quaternion.identity);
-        }
-
-        return null;
     }
 }
