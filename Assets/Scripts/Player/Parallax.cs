ce5ac46d5735c1fd497de6c5cb4e36d616d10cee
DevAkasha <hmy9025@gmail.com>
2025-02-25 15:08:42 +0900

    [fix] Parallax fix
    [fix] Parallax fix

--- Assets/Scripts/Player/Parallax.cs
+++ Assets/Scripts/Player/Parallax.cs
@@ -6,14 +6,14 @@ public class Parallax : MonoBehaviour
 {
     [SerializeField] private Vector2 prallaxEffectMultiplier;
 
-    private Transform mainCamera;
+    private Transform mainCameraTransform;
     private Vector3 lastCameraPosition;
     private float textureUnitSizeX;
 
     void Start()
     {
-        mainCamera = Camera.main.transform;
-        lastCameraPosition = mainCamera.position;
+        mainCameraTransform = Camera.main.transform;
+        lastCameraPosition = mainCameraTransform.position;
 
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
         Texture2D texture = sprite.texture;
@@ -23,15 +23,14 @@ public class Parallax : MonoBehaviour
 
     void LateUpdate()
     {
-        Vector3 deltamovement = mainCamera.position - lastCameraPosition;
+        Vector3 deltamovement = mainCameraTransform.position - lastCameraPosition;
         transform.position += new Vector3(deltamovement.x * prallaxEffectMultiplier.x, deltamovement.y * prallaxEffectMultiplier.y);
 
-        lastCameraPosition = mainCamera.position;
-
-        if (Mathf.Abs(mainCamera.position.x - transform.position.x) >= textureUnitSizeX)
+        if (Mathf.Abs(mainCameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
         {
-            float offsetPositionX = (mainCamera.position.x - transform.position.x) % textureUnitSizeX;
-            transform.position = new Vector3(mainCamera.position.x + offsetPositionX, transform.position.y);
+            float offsetPositionX = (mainCameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(mainCameraTransform.position.x + offsetPositionX, transform.position.y);
+            lastCameraPosition = mainCameraTransform.position;
         }
     }
 }
