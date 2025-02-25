4f1176582676de011d4fe16622dc4e9260db39d0
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-02-25 14:15:34 +0900

    [Update] Parallax

--- Assets/Scripts/Player/Parallax.cs
+++ Assets/Scripts/Player/Parallax.cs
@@ -8,11 +8,16 @@ public class Parallax : MonoBehaviour
 
     private Transform mainCamera;
     private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
 
     void Start()
     {
         mainCamera = Camera.main.transform;
         lastCameraPosition = mainCamera.position;
+
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
 
@@ -22,5 +27,11 @@ public class Parallax : MonoBehaviour
         transform.position += new Vector3(deltamovement.x * prallaxEffectMultiplier.x, deltamovement.y * prallaxEffectMultiplier.y);
 
         lastCameraPosition = mainCamera.position;
+
+        if (Mathf.Abs(mainCamera.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (mainCamera.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(mainCamera.position.x + offsetPositionX, transform.position.y);
+        }
     }
 }
