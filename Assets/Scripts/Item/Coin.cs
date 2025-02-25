12db4ca7568a9fd8931831b09a069f15b5182255
MTH_Inz <49317222+YujiHeo@users.noreply.github.com>
2025-02-25 19:48:01 +0900

    [Update] Item Effect /그런데 나중에 EffectScript 따로 만드는 편이 나을 듯

--- Assets/Scripts/Item/Coin.cs
+++ Assets/Scripts/Item/Coin.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class Coin : Item
 {
     public int scoreValue;
+    public GameObject itemEffect;
 
     protected override void ApplyEffect(PlayerAction player)
     {
         GameManager.Instance.AddScore(scoreValue);
+        PlayEffect();
     }
     protected override void ApplyEffect(PlayerAction player,int hitMultiplier)
     {
         GameManager.Instance.AddScore(scoreValue* hitMultiplier);
+        PlayEffect();
     }
-    
+
+    public GameObject PlayEffect()
+    {
+        if (itemEffect != null)
+        {
+            return Instantiate(itemEffect, transform.position, Quaternion.identity);
+        }
+
+        Destroy(itemEffect);
+
+        return null;
+
+
+    }
 }
