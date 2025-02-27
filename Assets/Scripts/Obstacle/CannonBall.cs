1ff34024e343f0b6cbdf778dc3f0d3b3747db02a
yunsoul9599 <ygh8366@naver.com>
2025-02-27 16:30:26 +0900

    [ Chore ] 하드 난이도 레벨 디자인 테스트

--- Assets/Scripts/Obstacle/CannonBall.cs
+++ Assets/Scripts/Obstacle/CannonBall.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CannonBall : Item
 {
-    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float speed = 2.0f;
 
     private void Start()
     {
